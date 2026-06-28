ff690cdeb911c82d7dab2efeed9656a414530ac4
Finn Dev <bigpower217th@gmail.com>
2026-06-28 16:51:32 +0300

    panic.c (fixing)

--- arch/x86/panic.c
+++ arch/x86/panic.c
@@ -1,13 +1,7 @@
 /*
  * ValiantCore Kernel
  * Copyright (C) 2026 bigpower
- *
- * This program is free software: you can redistribute it
- * and/or modify it under the terms of the GNU General
- * Public License as published by the Free Software
- * Foundation, version 3.
- *
- * SPDX-License-Identifier: GPL-3.0-only
+ * SPDX-License-Identifier: GPL-2.0-only
  */
 
 #include "../../include/kernel.h"
