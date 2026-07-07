938626e2bfbe0ff5ae9555c51f4a37a4a237287b
Finn Dev <finndev62@gmail.com>
2026-07-07 12:07:04 +0300

    ValiantCore Aarch64 SYNC_HANDLER

---
+++ arch/aarch64/sync_handler64.c
@@ -0,0 +1,13 @@
+/*
+* ValiantCore Kernel
+* Copyright (C) 2026 bigpower
+* SPDX-License-Identifier: GPL-2.0-only
+*/
+#include "include/kernel.h"
+#include <stdint.h>
+
+void sync_handler(uint64_t esr, uint64_t elr, uint64_t type) {
+     (void)type;
+     kernel_panic_aarch64(esr, elr);
+
+}
